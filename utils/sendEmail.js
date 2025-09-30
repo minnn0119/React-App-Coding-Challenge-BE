3b5468c4ca1eceb505c638af406a3434e4099bf6
minhdiep0119 <83383771+minnn0119@users.noreply.github.com>
2025-10-01 00:31:32 +0700

    Add Backend project

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,7 +4,6 @@ async function sendEmail(to, subject, html) {
   
   let testAccount = await nodemailer.createTestAccount();
 
-  // config transporter
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
@@ -16,16 +15,13 @@ async function sendEmail(to, subject, html) {
 
   });
 
-  // gửi mail
+
   const info = await transporter.sendMail({
     from: '"Test App" <test@example.com>',
     to,
     subject,
     html,
   });
-
-  console.log("✅ Email đã gửi thành công!");
-  console.log("MessageId:", info.messageId);
 
   return info;
 }
--- utils/sendSms.js
+++ utils/sendSms.js
@@ -1,13 +1,13 @@
 const twilio = require("twilio");
 
-const accountSid = "AC792b13fe585efaa8be45a6a721dcf2df";
-const authToken = "877a17e8481d97826efe8ac3009b1d95";
-const serviceSid = "VAb2682b27e696bddc8b9e4965e7cc9b83";
+const accountSid = "";
+const authToken = "";
+const serviceSid = "";
 
 const client = twilio(accountSid, authToken);
 
 async function sendSms(toPhone) {
-  if (!toPhone) throw new Error("❌ Phone number required");
+  if (!toPhone) throw new Error("Phone number required");
   return client.verify.v2.services(serviceSid).verifications.create({
     to: toPhone,
     channel: "sms",
